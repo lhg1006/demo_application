dc5d27f002958b625acebc86ad0adca6b521f668
이효규 <lhg961006@gmail.com>
2024-12-18 13:59:30 +0900

    init

---
+++ src/components/FileDropzone.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import React, { useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+
+const FileDropzone: React.FC = () => {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const onDrop = (acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+  };
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div>
+      <div {...getRootProps()} style={styles.dropzone}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+            <p>파일을 여기에 드롭하세요...</p> :
+            <p>파일을 드래그하거나 클릭하여 업로드하세요.</p>
+        }
+      </div>
+      <div>
+        <h2>드롭된 파일:</h2>
+        <ul>
+          {files.map((file, index) => (
+            <li key={index}>{file.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+const styles: { [key: string]: React.CSSProperties } = {
+  dropzone: {
+    border: '2px dashed #cccccc',
+    borderRadius: '5px',
+    padding: '20px',
+    textAlign: 'center',
+    cursor: 'pointer',
+    marginBottom: '20px'
+  }
+};
+
+export default FileDropzone;
